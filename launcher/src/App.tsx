b6e98fd2f7274bcec2b78ed489b1b2c2b7d031c4
Anth <sol9001@gmail.com>
2024-11-28 08:44:38 -0500

    rename to launcher

---
+++ launcher/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; 
+import Console from './Console'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Console /> {}
+    </div>
+  );
+}
+
+export default App;
